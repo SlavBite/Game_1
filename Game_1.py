9305fa10042d592618fa6a67aa8dc674b4f60994
SlavBite <64548708+SlavBite@users.noreply.github.com>
2020-06-12 19:23:09 +0500

    Update Game_1.py

--- Game_1.py
+++ Game_1.py
@@ -11,6 +11,8 @@ player = pygame.image.load('D:\\JustMonika\\_Game\\ara\\cat_right.png')
 icon = pygame.image.load('D:\\JustMonika\\_Game\\ara\\icon_1.png')
 pygame.mixer.music.load('D:\\JustMonika\\_Game\\ara\\music.mp3')
 pygame.mixer.music.set_volume(0.5)
+
+# health = pygame.transform.scale(image, (30,30)) ИЗМЕНЕНИЕ РАЗМЕРОВ КАРТИНКИ
 # Окно игры
 pygame.display.set_icon(icon)
 pygame.display.set_caption("Бегущие на месте 30 кг сала")
@@ -24,6 +26,7 @@ make_jump = False
 jump_count = 15
 scores = 0
 max_scores = 0
+
 
 
 # --------------------------
@@ -76,7 +79,28 @@ def draw_array_cactus(array_cactus): # Рисует кактусы
 			radius = find_radius(array_cactus)
 			cactus.return_self(radius)
 # ------------------------------
+# Класс отвечающий за создание кнопок
+class Button:
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+		self.inactive_color = (13,162,58)
+		self.active_color = (23,204,58)
 
+	def draw(self, x, y, message, font_size = 30, action=None):
+		mouse = pygame.mouse.get_pos()
+		click = pygame.mouse.get_pressed()
+
+		if x < mouse[0] < x +self.width:
+			if y < mouse[1] < y +self.height:
+				pygame.draw.rect(win, self.inactive_color, (x,y, self.width, self.height))
+				if click[0] == 1 and action is not None:
+					action()
+			else:
+				pygame.draw.rect(win, self.active_color, (x,y, self.width, self.height))		
+		else:
+			pygame.draw.rect(win, self.active_color, (x,y, self.width, self.height))
+		print_text(message = message, x = x+10, y = y+10, font_size = font_size)
 
 
 # Отрисовка всех объектов в окне
@@ -90,6 +114,7 @@ def drawWindow(array_cactus):
 	pygame.draw.rect(win,(255,255,255),(700,20,90,50))
 	scores += 1
 	print_text('Scores:' + str(scores), 600, 20)
+	button.draw(20, 100, 'Click', 15, prikol)
 
 
 	win.blit(player, (usr_x,usr_y)) # Отрисовка картинки 
@@ -98,38 +123,6 @@ def drawWindow(array_cactus):
 
 
 
-def run_game(): # Игра
-	game = True
-	global make_jump
-
-
-	pygame.mixer.music.play(-1)
-
-	array_cactus = []
-	create_cactus_arr(array_cactus)
-
-
-	while game:
-		drawWindow(array_cactus)
-		clock.tick(30)
-
-
-		for event in pygame.event.get(): # Выход из игры
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				quit()
-
-		keys = pygame.key.get_pressed() # Считывать все клавишы во время цикла
-		if keys[pygame.K_SPACE]:
-			make_jump = True
-		if keys[pygame.K_ESCAPE]:
-				pause()
-		if make_jump:
-			jump() 
-		if check_collision(array_cactus):
-			pygame.mixer.music.stop()
-			game = False
-	return game_over()
 
 
 
@@ -145,22 +138,18 @@ def jump(): # Прыжок игрока
 		make_jump = False
 		jump_count = 15
 
-def print_text(message, x, y, font_color = (0,0,0), font_type = 'D:\\JustMonika\\_Game\\ara\\langue.ttf', font_size = 30):
-	font_type = pygame.font.Font(font_type, font_size)
-	text = font_type.render(message, True, font_color)
-	win.blit(text, (x, y))
 
 def check_collision(barriers):
 	for barrier in barriers:
 		
-
+		# Простая и добрая система поиска коллисион с объектами
 		if usr_y + usr_height >= barrier.y:
 			if barrier.x <= usr_x <= barrier.x + barrier.width:
 				return True
 			elif barrier.x <= usr_x + usr_widht <= barrier.x + barrier.width:
 				return True
 
-		''' ОЧЕНЬ СЛОЖНАЯ ХУЙНЯ
+		''' ОЧЕНЬ СЛОЖНАЯ И НЕ РАБОЧАЯ УНЙЯХ
 		if barrier.height == 60:
 			if not make_jump:
 				if barrier.x <= usr_x + usr_widht - 6 <= barrier.x + barrier.width:
@@ -227,20 +216,84 @@ def game_over():
 			return True
 		if keys[pygame.K_ESCAPE]:
 			return False
+			scores = 0
+			return False
 		pygame.display.update()
 		clock.tick(15)
 
+def run_game(): # Игра
+	game = True
+	global make_jump
+
+
+	pygame.mixer.music.play(-1)
+
+	array_cactus = []
+	create_cactus_arr(array_cactus)
+
+
+	while game:
+		drawWindow(array_cactus)
+		clock.tick(30)
+
+
+		for event in pygame.event.get(): # Выход из игры
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+
+		keys = pygame.key.get_pressed() # Считывать все клавишы во время цикла
+		if keys[pygame.K_SPACE]:
+			make_jump = True
+		if keys[pygame.K_ESCAPE]:
+				pause()
+		if make_jump:
+			jump() 
+		if check_collision(array_cactus):
+			pygame.mixer.music.pause()
+			game = False
+	return game_over()
+
+def show_menu():
+	show = True
+	start_button = Button(300, 70)
+	while show:
+		clock.tick(60)
+		for event in pygame.event.get(): # Выход из игры
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+
+		win.fill((255,255,255))
+		start_button.draw(200, 200, 'Start game', 50 , main_game)
+		pygame.display.update()
+
+def print_text(message, x, y, font_color = (0,0,0), font_type = 'D:\\JustMonika\\_Game\\ara\\langue.ttf', font_size = 30):
+	font_type = pygame.font.Font(font_type, font_size)
+	text = font_type.render(message, True, font_color)
+	win.blit(text, (x, y))
+
+def main_game():
+	global make_jump, jump_count, usr_y
+
+	while run_game():
+		make_jump = False
+		jump_count = 15
+		usr_y = win_height - usr_height - 35
 
 
 
 
+button = Button(100,50)
+
+
+def prikol():
+	global usr_y
+	usr_y -= 10
 
 
 
-while run_game():
-	pass
+
+show_menu()
 pygame.quit()
 quit()
-
-
-
